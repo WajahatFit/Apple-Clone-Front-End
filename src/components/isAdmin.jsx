6e0d1fc5aedd5b47388a6e4a6bcfde264e94b9b0
Syed Wajahat Ali <wajahat.klk25.com>
2022-09-15 18:56:22 +0200

    create merges into dev

---
+++ src/components/isAdmin.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function IsAdmin({ children }) {
+  const { isAdmin } = useContext(AuthContext);
+  if (isAdmin) {
+    return children;
+  } else {
+    return <Navigate to="/" />;
+  }
+}
+
+export default IsAdmin;
